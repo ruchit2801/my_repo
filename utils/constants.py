0a48c8dd9820605f6ebd21d5589ef91d2c78a94a
ruchit2801 <vithaniruchit@gmail.com>
2021-02-12 14:25:07 +0530

    Updated constants

--- utils/constants.py
+++ utils/constants.py
@@ -2,4 +2,19 @@ from typing import List
 
 HIVE_SUPPORTED_DATA_TYPES: List[str] = ['tinyint', 'smallint', 'int', 'bigint', 'float', 'double', 'decimal', 'timestamp', 'date', 'string', 'varchar', 'char', 'boolean', 'binary', ]
 REQUIRED_KEYS: List[str] = ['schema_name', 'table_name', 'columns', 'input_format']
-DATA_TYPE_COMPATIBILITY_MAP = {}
+DATA_TYPE_COMPATIBILITY_MAP = {    
+    "boolean": {"boolean"},    
+    "tinyint": {"tinyint"},    
+    "smallint": {"tinyint", "smallint"},    
+    "int": {"tinyint", "smallint", "int"},    
+    "bigint": {"tinyint", "smallint", "int", "bigint"},    
+    "float": {"float"},    
+    "double": {"float", "double"},    
+    "decimal": {"decimal"},    
+    "varchar": {"varchar"},    
+    "timestamp": {"timestamp"},    
+    "date": {"date"},
+    "binary": {"binary"},
+    "string" : {"string"},
+    "char": {"char"}
+    }
